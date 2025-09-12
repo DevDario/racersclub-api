6ea90aba12c7837ce5cfd1a12188ea8953081253
Dário Silva <dariosilva13222@gmail.com>
2025-09-12 20:45:30 +0100

    inits project

---
+++ src/main/kotlin/com/devdario/racersclub/racersclubapi/RacersClubApiApplication.kt
@@ -0,0 +1,11 @@
+package com.devdario.racersclub.racersclubapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RacersClubApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RacersClubApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/devdario/racersclub/racersclubapi/RacersClubApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devdario.racersclub.racersclubapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RacersClubApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
